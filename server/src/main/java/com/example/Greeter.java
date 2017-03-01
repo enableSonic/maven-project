80bae9da69896ee303b8149790a87f3602195be1
enableSonic <ronnakrit.bu@gmail.com>
2017-03-01 17:09:01 +0700

    fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  /**
+  	* for Someone
+  	*
+  **/
+
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
